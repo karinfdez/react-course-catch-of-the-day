7258dc77ac0af8ccded776d5356815da6c0e40ee
karinfdez <karinfdez@yahoo.com>
2018-09-30 15:52:09 -0400

    Changing pages with React router. Handling events with onClick events.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,12 +3,16 @@ import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   //Creating properties of StorePicker (myInput and goToStore)
-  myInput = React.createRef();
+  myInput = React.createRef(); //To create a ref
 
   //Use this syntax to access component instance inside("this")
   goToStore = event => {
-    event.preventDefault(); //Stops the form from submitting
-    console.log(this);
+    //Stops the form from submitting
+    event.preventDefault();
+    //get data from user's input
+    const storeName = this.myInput.value.value;
+    //Change the page's router(Going to StorePicker we can see its props (history, location,  etc))
+    this.props.history.push(`/store/${storeName}`); //Because the StorePicker is the child of the Router it has access to its props
   };
 
   render() {
@@ -17,6 +21,7 @@ class StorePicker extends React.Component {
         {/* In order to add multiples childrens */}
         <form action="" className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
+          {/* Attach myInput ref  to React  elements via the ref attribute.*/}
           <input
             type="text"
             ref={this.myInput}
