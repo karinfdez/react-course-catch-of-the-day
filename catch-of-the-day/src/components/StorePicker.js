be32c56227424653a6ee3d03dde88ae12a015857
karinfdez <karinfdez@yahoo.com>
2018-09-30 15:05:04 -0400

    Adding events, refs and a way of binding this

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,24 @@ import React, { Fragment } from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  //Creating properties of StorePicker (myInput and goToStore)
+  myInput = React.createRef();
+
+  //Use this syntax to access component instance inside("this")
+  goToStore = event => {
+    event.preventDefault(); //Stops the form from submitting
+    console.log(this);
+  };
+
   render() {
     return (
       <Fragment>
-        {" "}
         {/* In order to add multiples childrens */}
-        <form action="" className="store-selector">
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Store Name"
             defaultValue={getFunName()}
