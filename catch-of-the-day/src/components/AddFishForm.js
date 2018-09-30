c38cc609225d96886bf1848dbf76b20723149a62
karinfdez <karinfdez@yahoo.com>
2018-09-30 17:45:11 -0400

    Create multiple reference to get data on form submit

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = event => {
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      image: this.imageRef.value.value
+    };
+    console.log(fish);
+  };
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" type="text" ref={this.nameRef} placeholder="Name" />
+        <input
+          name="price"
+          type="text"
+          ref={this.priceRef}
+          placeholder="Price"
+        />
+        <select name="status" ref={this.statusRef} placeholder="Status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" ref={this.descRef} placeholder="Desc" />
+        <input
+          name="image"
+          ref={this.imageRef}
+          type="text"
+          placeholder="Image"
+        />
+        <button type="submit">Add Fish</button>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
