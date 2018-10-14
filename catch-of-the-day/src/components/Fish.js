60896349e7914926d718f5081650401f6c988ae2
karinfdez <karinfdez@yahoo.com>
2018-10-13 20:13:13 -0400

    Use handleOrder functions as props to be add on button in onClick

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,8 +2,12 @@ import React from "react";
 import {formatPrice} from "../helpers";
 
 class Fish extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.key);
+  }
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,7 +16,7 @@ class Fish extends React.Component {
         ` <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button onClick = {this.handleClick} disabled={!isAvailable}>{isAvailable ? "Add To Oder" : "Sold Out!"}</button>
       </li>
     )
   }
