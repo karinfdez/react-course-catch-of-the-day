2cde30829084cbca4c0bd431a8dd14f037afd2c1
karinfdez <karinfdez@yahoo.com>
2018-10-21 13:41:12 -0400

    Add logic for bi-directional data flow

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -51,6 +51,17 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  updateFish = (key, updatedFish) => {
+    // 1. Take a copy of the current state
+    const fishes = { ...this.state.fishes };
+
+    // 2. Update that state
+    fishes[key] = updatedFish;
+
+    // 3. Set that to state
+    this.setState({fishes})
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -69,7 +80,9 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
